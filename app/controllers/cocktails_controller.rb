33110c47ef96cbe4f735e8091599c3abe4eab880
arnaud rottier <arnaud.rottier@gmail.com>
2020-02-21 16:20:52 +0900

    images added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,6 +3,7 @@ class CocktailsController < ApplicationController
 
   def index
     @cocktails = Cocktail.all
+    @path = ''
   end
 
   def show; end
@@ -23,7 +24,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
   def set_cocktail
